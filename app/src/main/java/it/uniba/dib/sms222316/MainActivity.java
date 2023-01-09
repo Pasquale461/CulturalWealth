827758f2531a02bc0a4d9dfd299b5de2024fcadc
Pasquale461 <p.manfredi3@studenti.uniba.it>
2023-01-09 17:38:18 +0100

    impostazione login e vincoli

--- app/src/main/java/it/uniba/dib/sms222316/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms222316/MainActivity.java
@@ -3,12 +3,66 @@ package it.uniba.dib.sms222316;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText Username , Password , Mail;
+    Button Confirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        setContentView(R.layout.activity_registration);
+
+
+        //Loginclass
+        Username = findViewById(R.id.editTextTextPersonName);
+        Mail = findViewById(R.id.editTextTextEmailAddress2);
+        Password = findViewById(R.id.editTextTextPassword);
+        Confirm = findViewById(R.id.confirm);
+
+        Confirm.setOnClickListener(v-> createAccount());
+    }
+    void createAccount(){
+        String name = Username.getText().toString();
+        String mail = Mail.getText().toString();
+        String pass = Password.getText().toString();
+        boolean isValid = validatedata(name, mail, pass);
+        if(!isValid)return;
+        createAccountInFirebase(name, mail, pass);
+    }
+
+
+
+    void createAccountInFirebase(String Name , String Email , String Password_local)
+    {
+
+    }
+
+    void changeInProgress(boolean inProgress)
+    {
+
+    }
+
+    boolean validatedata(String Name , String Email , String Password_local){
+        if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches())
+        {
+            Mail.setError("Email is invalid");
+            return false;
+        }
+        if(Password_local.length()<6)
+        {
+            Password.setError("Password length is invalid");
+            return false;
+        }
+        return true;
     }
 }
+
---
+++ app/src/main/java/it/uniba/dib/sms222316/registration.java
@@ -0,0 +1,20 @@
+package it.uniba.dib.sms222316;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class registration extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+
+    }
+}
